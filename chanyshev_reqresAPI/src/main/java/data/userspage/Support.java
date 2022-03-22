86f6dca82347ac6fb27aa6142d36974bc45b36a1
chanyshev92 <chanyshev92@gmail.com>
2022-03-22 16:45:40 +0500

    Support.java

--- chanyshev_reqresAPI/src/main/java/data/userspage/Support.java
+++ chanyshev_reqresAPI/src/main/java/data/userspage/Support.java
@@ -1,14 +1,10 @@
 
 package data.userspage;
 
+import com.fasterxml.jackson.annotation.*;
+
 import java.util.HashMap;
 import java.util.Map;
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 /**
  * Класс с сопутствующими данными на page/users?page=
